1f36f17f15c7b090a918d4fff9e677cf34046bb0
Na Hyung Choi <schoinh@gmail.com>
2020-05-05 16:25:03 -0700

    Make xylophone work

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+		sender.alpha = 0.5
+		playSound(note: sender.currentTitle!)
+		DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(200)) {
+			sender.alpha = 1
+		}
+    }
     
-
+	func playSound(note: String) {
+        let url = Bundle.main.url(forResource: note, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
